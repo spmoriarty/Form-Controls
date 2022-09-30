caa76fbe88f7034d6b7476928872e36713b2e4ad
Sean Moriarty <spmoriarty1@gmail.com>
2022-09-30 11:38:43 -0700

    added in form and input control with test

---
+++ src/components/Forms/FormControls.test.jsx
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react';
+
+import { 
+  InputController,
+
+} from './FormControls';
+
+test('INPUT CONTROL', async () => {
+  render(
+    <InputController
+      label="User Name"
+      name="username"
+      required
+      placeholder="Your user name"
+    />
+  );
+  const inputController = screen.getByLabelText('User Name');
+  expect(inputController.name).toBe('username');
+  expect(inputController.placeholder).toBe('Your user name');
+  expect(inputController.required).toBe(true);
+  
+});
